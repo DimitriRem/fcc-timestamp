95e51ebbf87db9335270770745d2626339fdeea3
Dimitri Remoundos <dimitri.remoundos@gmail.com>
2024-03-13 09:46:09 +0000

    test

--- index.js
+++ index.js
@@ -29,55 +29,73 @@ app.get("/api/:date?", function (req, res) {
 
   console.log(".get Start");
   const dateEntered= req.params.date;
-  let parsedDate=null;
-  let stdDate=null;
-  const errorMsg = {error: "Invalid Date"}
+   let stdDate=null;
 
-  if (req.params.date == null) {
-    console.log("01 - null value");
-    parsedDate = new Date().getTime();
+   if (!dateEntered) {
     stdDate = new Date();
-  } 
+   }
 
-  else if (req.params.date.length > 10) {
-    console.log("03 - unix date entered");
-    parsedDate = new Date(parseInt(req.params.date)).getTime();
-    stdDate = new Date(parseInt(req.params.date));
-    console.log("03.5 p:"+parsedDate+" std: "+stdDate);
-  } else {
-    console.log("04 - std date entered");
-     parsedDate = new Date(req.params.date).getTime();
-     stdDate = new Date(req.params.date);
-     if (!isNaN(parsedDate)) { 
-     console.log("04.5 date ok " + parsedDate) } else {
-     console.log("04.5 date kak " + parsedDate)
-     return res.json(errorMsg);};
+   else if (dateEntered.length > 10) {
+    stdDate = new Date(parseInt(dateEntered));
+   }
+
+   else {
+    stdDate = new Date(dateEntered);
+   }
+
+   if (isNaN(stdDate)) {
+    return res.json({error: "Invalid Date"})
+   }
+
+   else {
+    return res.json({unix: stdDate.getTime(), uts: stdDate.toUTCString()})
+   }
+
+//   if (req.params.date == null) {
+//     console.log("01 - null value");
+//     parsedDate = new Date().getTime();
+//     stdDate = new Date();
+//   } 
+
+//   else if (req.params.date.length > 10) {
+//     console.log("03 - unix date entered");
+//     parsedDate = new Date(parseInt(req.params.date)).getTime();
+//     stdDate = new Date(parseInt(req.params.date));
+//     console.log("03.5 p:"+parsedDate+" std: "+stdDate);
+//   } else {
+//     console.log("04 - std date entered");
+//      parsedDate = new Date(req.params.date).getTime();
+//      stdDate = new Date(req.params.date);
+//      if (!isNaN(parsedDate)) { 
+//      console.log("04.5 date ok " + parsedDate) } else {
+//      console.log("04.5 date kak " + parsedDate)
+//      return res.json(errorMsg);};
     
-  }
+//   }
 
-const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+// const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+// const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-const utcDate = `${days[stdDate.getUTCDay()]}, ${stdDate.getUTCDate()} ${months[stdDate.getUTCMonth()]} ${stdDate.getUTCFullYear()} ${('0' + stdDate.getUTCHours()).slice(-2)}:${('0' + stdDate.getUTCMinutes()).slice(-2)}:${('0' + stdDate.getUTCSeconds()).slice(-2)} GMT`;
+// const utcDate = `${days[stdDate.getUTCDay()]}, ${stdDate.getUTCDate()} ${months[stdDate.getUTCMonth()]} ${stdDate.getUTCFullYear()} ${('0' + stdDate.getUTCHours()).slice(-2)}:${('0' + stdDate.getUTCMinutes()).slice(-2)}:${('0' + stdDate.getUTCSeconds()).slice(-2)} GMT`;
 
-  // 
+//   // 
 
- const options = {
-    weekday: 'short',
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-    hour12: false, // Use 24-hour format
-    timeZone: 'UTC'  // Format in UTC
-  };
+//  const options = {
+//     weekday: 'short',
+//     year: 'numeric',
+//     month: 'short',
+//     day: 'numeric',
+//     hour: 'numeric',
+//     minute: 'numeric',
+//     second: 'numeric',
+//     hour12: false, // Use 24-hour format
+//     timeZone: 'UTC'  // Format in UTC
+//   };
 
 
-  // const utcDate = new Date(parsedDate).toLocaleDateString('en-ZA', options) + ' GMT';
-  const dateObject = {unix:parsedDate, utc:utcDate};
-  res.json(dateObject);
+//   // const utcDate = new Date(parsedDate).toLocaleDateString('en-ZA', options) + ' GMT';
+//   const dateObject = {unix:parsedDate, utc:utcDate};
+//   res.json(dateObject);
 } );
 
 
